715336e416dad36a5aa850dad2a8bae0bab6d628
Levelling-up-everyday <106878532+Levelling-up-everyday@users.noreply.github.com>
2022-11-10 23:15:14 +0530

    Create 02.Consecutive_string.cpp

---
+++ 02.Consecutive_string.cpp
@@ -0,0 +1,4 @@
+//Easy-level
+//if two adjacent letters are same remove it
+//ex:abbaca ans aa
+//Apporach take a stack /string ans and keep pushing elements if it is not equal to the top of the stack if it is equal just pop and final stack contents are its result
