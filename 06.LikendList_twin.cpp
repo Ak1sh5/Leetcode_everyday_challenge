fa5e14c2644494d4301beda9c433110c6cc3179e
Levelling-up-everyday <106878532+Levelling-up-everyday@users.noreply.github.com>
2023-05-17 10:59:15 +0530

    Update 06.LikendList_twin.cpp

--- 06.LikendList_twin.cpp
+++ 06.LikendList_twin.cpp
@@ -1,2 +1,8 @@
 https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/description/
-
+Did with array apporach
+But optimal is:
+TAke slow and fast pointer and find the middle element
+Then reverse the second half of the linked list
+Then just traverse together and find the max sum and return
+  
+ Havent completed yet since idk reversal of linked list yet!.
